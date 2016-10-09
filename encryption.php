f1e276fbaff919f5551f00bcbeff57f4d2122ac0
cvexa <c_v_e_x_a@abv.bg>
2016-10-10 01:46:53 +0300

    working algorithm
    its very basic but working, encrypting the words, and not encrypt the
    SPACE " , not sure about this + some basic checks

--- encryption.php
+++ encryption.php
@@ -8,13 +8,16 @@ include_once("includes/header.html");
 			<h2>Encryption</h2>
 			<span class="byline">Enter some text to encrypt, and shift :</span> </div>
 			<p>Example: How are You today ? | 5 </p>
-		<p> <form id="frm1" action="encryption.php">
-  <p>TEXT: <input type="text" name="text"></p>
-  <p>SHIFT: <input type="number" name="shift"></p>
+		<p><form id="frm1" action="encryption.php" method="post">
+  <p>TEXT: <input id="text" type="text" name="text"></p>
+ <p> SHIFT: <input id="shift"type="number" name="shift"></p>
   
   <input type="button" onclick="myFunction()" value="Submit" class="button">
 </form> </p>
-
+<p id = "final">
+	
+</p>
+<input type = "button" onclick = "refresh()" value="refresh" class = "button">
 			
 		
 	</div>
--- js/test1.js
+++ js/test1.js
@@ -1,6 +1,44 @@
 function myFunction() {
-    document.getElementById("frm1").submit();
- 
-    alert('Still in progress !');
+
+    var a = document.getElementById("text").value;
+    var b = document.getElementById("shift").value;
+    var final = " ";
+
+    if (a === "") {
+    	alert('Please enter some text ....');
+    }else if(b === "" || b === "0"){
+    	alert('Please enter some shift ....');
+    }else{
+
+   for (var i = 0;i <= a.length - 1;i++) {
+    var c = a[i].charCodeAt(0);
+    var parseC = parseInt(c);
+    // console.log(parseC);
+    var parseB = parseInt(b);
+    if (parseC === 32) {
+    var convert = parseC;
+    var result = String.fromCharCode(convert);
+	final += result;
+    }else{
+	convert = parseC += parseB;
+	result = String.fromCharCode(convert);
+	final += result;
+	// alert(result);
+	}
+}
+}
+ // console.log(final);
+
+
+var element = document.getElementById("final").innerHTML += "Encrypted text with "+ b + " shift :"+ final;
+var element2 = document.getElementById("final").style.color="#85c1a4";
+
+// window.location.reload();
+
+
+}
+function refresh(){
+	element = document.getElementById("final").innerHTML = " ";
+	window.location.reload();
 
 }
